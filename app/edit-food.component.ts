b4b4199c0bb5a20624f0c63a41ae2cde7f694b61
Jessica Fix <jcfix.pdx@gmail.com>
2016-03-25 12:28:32 -0700

    Add method to update Calories on edit for a calorie calc

--- app/edit-food.component.ts
+++ app/edit-food.component.ts
@@ -1,19 +1,29 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
 import {Food} from './food.model';
 
 @Component ({
   selector: 'edit-food',
   inputs: ['food'],
+  outputs: ['onUpdateCaloricIntake'],
   template: `
   <div class="food-form">
     <input [(ngModel)]="food.name" class="form-control"/>
     <input [(ngModel)]="food.description" class="form-control"/>
-    <input [(ngModel)]="food.calories" type="number" class="form-control">
+    <input [(ngModel)]="food.calories" type="number" class="form-control" #newCalories>
+    <button (click)="updateCaloricIntake(newCalories)" class="btn btn-info">Update</button>
   </div>
   `
 })
 
 export class EditFoodComponent {
   public food: Food;
+  public onUpdateCaloricIntake: EventEmitter<number>;
+  constructor() {
+    this.onUpdateCaloricIntake = new EventEmitter();
+  }
+  updateCaloricIntake(inputCalories: HTMLInputElement): void {
+    var newCal: number = parseInt(inputCalories.value);
+    this.onUpdateCaloricIntake.emit(newCal);
+  }
 
 }
