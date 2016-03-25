73e9f19993ea05fd8896c51403d6b35cf00d7ee9
Jessica Fix <jcfix.pdx@gmail.com>
2016-03-25 12:10:22 -0700

    Add pipe to food-list

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -3,13 +3,20 @@ import { FoodComponent } from './food.component';
 import { Food } from './food.model';
 import { NewFoodComponent } from './new-food.component';
 import { EditFoodComponent } from './edit-food.component';
+import { CaloriesPipe } from './calories.pipe';
 
 @Component({
   selector: 'food-list',
   inputs: ['foodList'],
   directives: [FoodComponent, NewFoodComponent, EditFoodComponent],
+  pipes: [CaloriesPipe],
   template: `
-  <div *ngFor="#currentFood of foodList">
+  <select (change)="onChange($event.target.value)" class="form-control" id="dropdown">
+      <option value="all" selected="selected">All Foods</option>
+      <option value="healthy">Healthy Foods</option>
+      <option value="unhealthy">Unhealthy Foods</option>
+    </select>
+  <div *ngFor="#currentFood of foodList | calories: filterCalories">
     <h4 class="foodListItem" (click)="clickFood(currentFood)">
     {{ currentFood.name }}
     </h4>
@@ -24,6 +31,7 @@ import { EditFoodComponent } from './edit-food.component';
 export class FoodListComponent {
   public foodList: Food[];
   public selectedFood: Food;
+  public filterCalories: string = 'all';
   constructor() {}
   clickFood(clickedFood: Food): void {
     if(this.selectedFood === clickedFood) {
@@ -36,5 +44,8 @@ export class FoodListComponent {
     this.foodList.push(
       new Food(newFoodArr[0], newFoodArr[1], newFoodArr[2])
     );
+  }
+  onChange(filterOption) {
+    this.filterCalories = filterOption;
   }
 }
